fcd2df2b46b910e490e8144d96e9223cb3a5a6a4
arman-borkhani <armanborkhani@gmail.com>
2024-10-29 21:02:22 +0330

    Implement dark mode

---
+++ src/components/DarkModeSwitch.tsx
@@ -0,0 +1,9 @@
+import { ColorModeButton } from "./ui/color-mode"
+
+const DarkModeSwitch = () => {
+  return (
+    <ColorModeButton />
+  )
+}
+
+export default DarkModeSwitch
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,16 @@
 import { Container, HStack, Image, Text } from "@chakra-ui/react"
 import logo from "../assets/logo.webp"
+import DarkModeSwitch from "./DarkModeSwitch"
 
 const NavBar = () => {
   return (
     <Container fluid>
-      <HStack>
-        <Image src={logo} boxSize="60px" />
-        <Text>GameHub</Text>
+      <HStack justifyContent="space-between">
+        <HStack>
+          <Image src={logo} boxSize="60px" />
+          <Text>GameHub</Text>
+        </HStack>
+        <DarkModeSwitch />
       </HStack>
     </Container>
   )
