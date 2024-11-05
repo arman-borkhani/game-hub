b35ee54d382443516d547f5c6a2577a5d9043fb8
arman-borkhani <armanborkhani@gmail.com>
2024-11-05 21:04:40 +0330

    Show loading skeleton

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,14 +1,18 @@
 import useGames from "@/hooks/useGames"
 import { Grid, Text } from "@chakra-ui/react"
 import GameCard from "./GameCard"
+import GameCardSkeleton from "./GameCardSkeleton"
 
 const GameGrid = () => {
-  const {games, error} = useGames();
+  const {games, error, isLoading} = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <Grid templateColumns={{ sm: "repeat(1, 1fr)", md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }} gap="6" paddingX="24px">
+        {isLoading &&
+          skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
         {games.map(game => (
           <GameCard key={game.id} game={game} />
         ))}
