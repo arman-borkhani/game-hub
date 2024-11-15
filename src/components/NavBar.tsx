ca4a0c3a48d4c0181f1eef29e8a12a00f2524053
arman-borkhani <armanborkhani@gmail.com>
2024-11-15 18:34:03 +0330

    Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,17 @@
 import { Container, HStack, Image, Text } from "@chakra-ui/react"
 import logo from "../assets/logo.webp"
 import DarkModeSwitch from "./DarkModeSwitch"
+import SearchInput from "./SearchInput"
 
 const NavBar = () => {
   return (
     <Container fluid>
-      <HStack justifyContent="space-between">
-        <HStack>
+      <HStack gap={4} >
+        <HStack flexShrink={0}>
           <Image src={logo} boxSize="60px" />
           <Text>GameHub</Text>
         </HStack>
+        <SearchInput />
         <DarkModeSwitch />
       </HStack>
     </Container>
