3e954fc1723fb1c22e0e4621aaf46f4c5939ae3a
arman-borkhani <armanborkhani@gmail.com>
2024-11-17 20:50:10 +0330

    Add heading for genre list component

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,6 @@
 import useGenre, { Genre } from "@/hooks/useGenre"
 import getCroppedImageUrl from "@/services/image-url"
-import { HStack, Image, Link, List } from "@chakra-ui/react"
+import { Heading, HStack, Image, Link, List } from "@chakra-ui/react"
 import GenreSkeleton from "./GenreSkeleton"
 
 interface Props {
@@ -13,18 +13,21 @@ const GenreList = ({onSelectGenre, selectedGenre}: Props) => {
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
-    <List.Root variant="plain" display="flex" flexDir="column" gap={3}>
-      {isLoading &&
-        skeletons.map(skeleton => <List.Item key={skeleton}><GenreSkeleton /></List.Item>)}
-      {data.map(genre => (
-        <List.Item key={genre.id}>
-          <HStack>
-            <Image boxSize="32px" borderRadius={8}  src={getCroppedImageUrl(genre.image_background)} />
-            <Link fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal' } onClick={() => onSelectGenre(genre)} fontSize="lg" variant="plain">{genre.name}</Link>
-          </HStack>
-        </List.Item>
-      ))}
-    </List.Root>
+    <>
+      <Heading size="xl" marginBottom={3}>Genres</Heading>
+      <List.Root variant="plain" display="flex" flexDir="column" gap={3}>
+        {isLoading &&
+          skeletons.map(skeleton => <List.Item key={skeleton}><GenreSkeleton /></List.Item>)}
+        {data.map(genre => (
+          <List.Item key={genre.id}>
+            <HStack>
+              <Image boxSize="32px" borderRadius={8}  src={getCroppedImageUrl(genre.image_background)} />
+              <Link fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal' } onClick={() => onSelectGenre(genre)} fontSize="lg" variant="plain">{genre.name}</Link>
+            </HStack>
+          </List.Item>
+        ))}
+      </List.Root>
+    </>
   )
 }
 
