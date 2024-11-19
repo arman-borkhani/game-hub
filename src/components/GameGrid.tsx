0aea269b202b19733a37b8753e8b13d0d36fb094
arman-borkhani <armanborkhani@gmail.com>
2024-11-19 21:36:58 +0330

    Refactor game grid component

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,6 +1,7 @@
 import useGames from "@/hooks/useGames"
 import { GameQuery } from "@/App"
-import { Grid, Text } from "@chakra-ui/react"
+import { Grid } from "@chakra-ui/react"
+import { Alert } from "@/components/ui/alert"
 import GameCard from "./GameCard"
 import GameCardSkeleton from "./GameCardSkeleton"
 
@@ -12,17 +13,16 @@ const GameGrid = ({gameQuery}: Props) => {
   const {data, error, isLoading} = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Alert status="error" title={error}></Alert>
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <Grid templateColumns={{ sm: "repeat(1, 1fr)", md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }} gap="6">
-        {isLoading &&
-          skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
-        {data.map(game => (
-          <GameCard key={game.id} game={game} />
-        ))}
-      </Grid>
-    </>
+    <Grid templateColumns={{ sm: "repeat(1, 1fr)", md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }} gap="6">
+      {isLoading &&
+        skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
+      {data.map(game => (
+        <GameCard key={game.id} game={game} />
+      ))}
+    </Grid>
   )
 }
 
