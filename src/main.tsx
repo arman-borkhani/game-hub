5423bdc7f0e8ece221e01ba34cd8efae802f9804
arman-borkhani <armanborkhani@gmail.com>
2024-12-02 21:24:16 +0330

    Add react query package

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import { Provider } from "@/components/ui/provider"
 import { ColorModeProvider } from "@/components/ui/color-mode"
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
       <ColorModeProvider>
+      <QueryClientProvider client={queryClient}>
         <App />
+      </QueryClientProvider>
       </ColorModeProvider>
     </Provider>
   </StrictMode>,
