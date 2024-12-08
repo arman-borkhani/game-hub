335f4bfdf58d99193196d596ad393667c4d55b66
arman-borkhani <armanborkhani@gmail.com>
2024-12-08 20:43:56 +0330

    Refactor useGames hook with react query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,13 +13,13 @@ const GameGrid = ({gameQuery}: Props) => {
   const {data, error, isLoading} = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Alert status="error" title={error}></Alert>
+  if (error) return <Alert status="error" title={error.message}></Alert>
 
   return (
     <Grid templateColumns={{ sm: "repeat(1, 1fr)", md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }} gap="6">
       {isLoading &&
         skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
-      {data.map(game => (
+      {data?.results.map(game => (
         <GameCard key={game.id} game={game} />
       ))}
     </Grid>
