44104fe51560afe4fb9d97e193c5e1aac8c8a2f8
arman-borkhani <armanborkhani@gmail.com>
2024-12-25 10:18:19 +0330

    Setup routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from "@/components/ui/provider"
 import { ColorModeProvider } from "@/components/ui/color-mode"
 import './index.css'
-import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes'
+
 
 const queryClient = new QueryClient()
 
@@ -13,7 +15,7 @@ createRoot(document.getElementById('root')!).render(
     <Provider>
       <ColorModeProvider>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
       </QueryClientProvider>
       </ColorModeProvider>
     </Provider>
