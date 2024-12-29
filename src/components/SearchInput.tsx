5879dda505dddcd935f3a430631a282bfc308660
arman-borkhani <armanborkhani@gmail.com>
2024-12-29 21:01:45 +0330

    Navigate to home page after search form submit

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,15 +3,21 @@ import { Input } from "@chakra-ui/react"
 import { InputGroup } from "./ui/input-group"
 import { BsSearch } from "react-icons/bs"
 import useGameQueryStore from "@/store"
+import { useNavigate } from "react-router-dom"
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore(s => s.setSearchText);
+  const navigate = useNavigate();
 
   return (
     <form onSubmit={(event) => {
       event.preventDefault();
-      if(ref.current) setSearchText(ref.current.value);
+      if(ref.current) {
+        setSearchText(ref.current.value);
+        navigate('/');
+      }
+
     }}>
       <InputGroup width="100%" startElement={<BsSearch />}>
         <Input ref={ref} borderRadius={20} placeholder="Search games..." variant="subtle" />
