fa3cd1e8b306a9244a1cf8aeb85437d357dd3664
arman-borkhani <armanborkhani@gmail.com>
2024-12-29 20:56:30 +0330

    Link navbar logo to home page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,16 @@ import { Box, Container, HStack, Image } from "@chakra-ui/react"
 import logo from "../assets/logo.png"
 import DarkModeSwitch from "./DarkModeSwitch"
 import SearchInput from "./SearchInput"
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
   return (
     <Box borderBottomWidth="1px">
       <Container fluid>
         <HStack gap={4} paddingY={4}>
-          <Image src={logo} boxSize="60px" />
+          <Link to="/">
+            <Image src={logo} boxSize="60px" />
+          </Link>
           <SearchInput />
           <DarkModeSwitch />
         </HStack>
