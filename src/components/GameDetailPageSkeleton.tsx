843a8db150edc0a221b86f321cfb3ad32621d108
arman-borkhani <armanborkhani@gmail.com>
2024-12-30 12:36:39 +0330

    Change game detail page skeleton

--- src/components/GameDetailPageSkeleton.tsx
+++ src/components/GameDetailPageSkeleton.tsx
@@ -9,9 +9,12 @@ const GameDetailPageSkeleton = () => {
           <Skeleton height="10" maxWidth="400px" />
           <SkeletonText noOfLines={3} gap="4" />
         </Flex>
-        <Flex direction="column" gap="4">
-          <Skeleton height="400px" />
-        </Flex>
+        <SimpleGrid columns={{base: 1, md: 2}} gap={2}>
+          <Skeleton height="240px" />
+          <Skeleton height="240px" />
+          <Skeleton height="240px" />
+          <Skeleton height="240px" />
+        </SimpleGrid>
       </SimpleGrid>
     </Container>
   )
